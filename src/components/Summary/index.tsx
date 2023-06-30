1cbced91077e56615d54a46bd587327a35ce78cb
Angelica Resende Araujo <angelresende007@gmail.com>
2023-06-30 17:16:31 -0300

    table style

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,32 +1,32 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
-import { SummaryContainer } from "./style";
+import { SummaryContainer, SummaryCard } from "./style";
 
 export function Summary(){
   return(
     <SummaryContainer>
-      <div>
+      <SummaryCard>
         <header>
           <span>Entradas</span>
           <ArrowCircleUp size={32} color="#00b37e"/>
         </header>
         <strong>R$ 17.400,00</strong>
-      </div>
+      </SummaryCard>
 
-      <div>
+      <SummaryCard>
         <header>
           <span>Saídas</span>
           <ArrowCircleDown size={32} color="#f75a68"/>
         </header>
         <strong>R$ 17.400,00</strong>
-      </div>
+      </SummaryCard>
 
-      <div>
+      <SummaryCard variant="green">
         <header>
           <span>Total</span>
           <CurrencyDollar size={32} color="#fff"/>
         </header>
         <strong>R$ 17.400,00</strong>
-      </div>
+      </SummaryCard>
     </SummaryContainer>
   )
 }
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighLight, TransactionContainer, TransactionTable } from "./style";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionContainer>
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td><PriceHighLight variant="income">R$ 12.000,00</PriceHighLight></td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td><PriceHighLight variant="outcome">- R$ 120,00</PriceHighLight></td>
+              <td>Alimentação</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td><PriceHighLight variant="income">R$ 12.000,00</PriceHighLight></td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionTable>
+      </TransactionContainer>
     </div>
   )
 }
