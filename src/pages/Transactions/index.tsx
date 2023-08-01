38da86a4fc8b407a7fcbc19f300aaadf6e495280
Angelica Resende <angelica.resende@ufenesp.com.br>
2023-08-01 17:57:17 -0300

    atualização código conex api

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,36 +1,41 @@
-import { Header } from "../../components/Header";
-import { Summary } from "../../components/Summary";
-import { PriceHighLight, TransactionContainer, TransactionTable } from "./style";
+import { useContext } from 'react'
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
+import { SearchForm } from './components/SearchForm'
+import { PriceHighLight, TransactionContainer, TransactionTable } from './style'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
 
 export function Transactions() {
+  const { transactions } = useContext(TransactionsContext)
+
   return (
     <div>
       <Header />
       <Summary />
 
       <TransactionContainer>
+        <SearchForm />
+
         <TransactionTable>
           <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td><PriceHighLight variant="income">R$ 12.000,00</PriceHighLight></td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Hamburguer</td>
-              <td><PriceHighLight variant="outcome">- R$ 120,00</PriceHighLight></td>
-              <td>Alimentação</td>
-              <td>13/04/2022</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td><PriceHighLight variant="income">R$ 12.000,00</PriceHighLight></td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighLight variant={transaction.type}>
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
+                    </PriceHighLight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>
+                    {dateFormatter.format(new Date(transaction.createdAt))}
+                  </td>
+                </tr>
+              )
+            })}
           </tbody>
         </TransactionTable>
       </TransactionContainer>
